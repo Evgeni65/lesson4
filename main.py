3128b32497d2b356c06d172729acda954255fe12
Evgeni65 <evgenikorolev56@gmail.com>
2024-09-28 16:37:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+neme = 'Python'
+DZ = 12
+time = 1.5
+print('Курс:',neme,',','всего задач:',DZ,',''затрачено часов:',time,',''среднее время выполнеия:',time/DZ,'.')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
